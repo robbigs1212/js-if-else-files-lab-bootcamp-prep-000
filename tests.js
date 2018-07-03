27d2623fa0ea8becc21b502eafcc1982275513f7
ROB BIRMINGHAM <robbigs@gmail.com>
2018-07-03 00:07:53 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,14 +1,14 @@
 if (index.name === "Susan") {
-    console.log("The name is correct");
+    console.log("The name is correct")
 } else {
-    console.log("Expected: Susan, Received: "+index.name);
+    console.log("Expected: Susan, Received: "+index.name)
 }
 
 
 
 
 if (index.height === 70) {
-    console.log("The height is correct");
+    console.log("The height is correct")
 } else {
-    console.log("Expected: 70, Received: "+index.height);
+    console.log("Expected: 70, Received: "+index.height)
 }
