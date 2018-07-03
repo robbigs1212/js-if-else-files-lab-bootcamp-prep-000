9ac8f07f029a436b9a318d216f5aec01dee64086
ROB BIRMINGHAM <robbigs@gmail.com>
2018-07-03 00:01:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var height = "74"
  
  if (name==="Susan") {
     console.log("Correct")
+    } else {
+      console.log("Expected")
     }
+}
 // Don't worry about this
 module.exports = { name, height
 }
