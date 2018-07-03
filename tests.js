94fd17009d1e9b35ecbc9a22c5dffb76a1e47f72
ROB BIRMINGHAM <robbigs@gmail.com>
2018-07-03 00:43:55 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,5 +1,5 @@
 var index = require("./index.js")
-if (index.name === "") {
+if (index.name === "Joe") {
     console.log("The name is correct")
 } else {
     console.log("Expected: Susan, Received: "+index.name)
