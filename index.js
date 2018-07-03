3131cd755431b865fff6db7c2cc84fa5a40a99de
ROB BIRMINGHAM <robbigs@gmail.com>
2018-07-03 00:33:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var name = "Joe";
 var height = "74";
  
+var name = document.querySelector("name");
+name.innerHTML ="Susan"
+
+var height = document.querySelector("height")
+height.innerHTML = parseInt(height.innerHTML) - 4
 // Don't worry about this
 module.exports = { name, height
 };
